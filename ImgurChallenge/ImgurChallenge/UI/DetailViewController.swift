a588438a2bc8b9027d9962d2d36fac3295142064
MdBee <matt_bearson@hotmail.com>
2019-04-14 20:39:08 -0700

    Zooming and activity indicator.

--- ImgurChallenge/ImgurChallenge/UI/DetailViewController.swift
+++ ImgurChallenge/ImgurChallenge/UI/DetailViewController.swift
@@ -9,60 +9,59 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
-//            if let label = detailDescriptionLabel {
-//                label.text = detail.dateTime?.description
-//            }
             self.title = detail.title
-            //self.activityIndicator.isHidden = false
-            
+            self.activityIndicator?.isHidden = false
             
             if detail.imageData != nil {
                 self.imageView?.image = UIImage(data: detail.imageData!)
+                self.activityIndicator?.isHidden = true
             } else {
-                
                 guard let link = detail.imageLink
-                    else { print("no image link")
-                        return
-                }
+                    else { print("no image link"); return}
                 if let imgURL = URL.init(string: link) {
                     do {
                         let imageData = try Data(contentsOf: imgURL as URL);
                         let image = UIImage(data: imageData);
+                        DispatchQueue.main.async {
                         self.imageView?.image = image
+                        self.activityIndicator?.isHidden = true
+                        }
                         
                         //update Item with imageData
                         detail.imageData = imageData
                     } catch {
-                        self.imageView?.image = UIImage(named: "JohnWayne")
                         print("Unable to load data: \(error)")
                     }
                 }
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.scrollView.delegate = self
         configureView()
     }
-
+    
     var detailItem: Item? {
         didSet {
-            // Update the view.
             configureView()
         }
     }
-
-
 }
 
+extension DetailViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+    }
+}
