1ca6e469fc7839d7c5ac2a6208f1e95fc5690465
MdBee <matt_bearson@hotmail.com>
2019-04-16 21:13:11 -0700

    Added a few tests.

--- ImgurChallenge/ImgurChallengeUITests/ImgurChallengeUITests.swift
+++ ImgurChallenge/ImgurChallengeUITests/ImgurChallengeUITests.swift
@@ -26,9 +26,16 @@ class ImgurChallengeUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testToolbarButtonTitleToggle() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let sut = XCUIApplication()
+
+        let nsfwButton = sut.toolbars["Toolbar"].buttons["nsfw is filtered out"]
+        nsfwButton.tap()
+        
+        // If title has changed button won't be found.
+        XCTAssertFalse(nsfwButton.exists)
     }
 
 }
